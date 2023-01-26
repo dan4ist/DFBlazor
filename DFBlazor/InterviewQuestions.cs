5cdda010444fc2fd7cc42b191693a7994da64420
danie <danie@DESKTOP-D1IRDPB>
2023-01-26 10:01:45 -0500

    Add project files.

---
+++ DFBlazor/InterviewQuestions.cs
@@ -0,0 +1,33 @@
+﻿namespace DFBlazor {
+    public class InterviewQuestions {
+
+        public int[] TwoSum(int[] arr, int target) {
+            Dictionary<int, int> indices = new Dictionary<int, int>();
+            for ( int i = 0; i < arr.Length; i++ ) {
+                int num = arr[i];
+                if ( indices.ContainsKey(target - num) ) {
+                    return new int[] { indices[target - num], i };
+                }
+                indices[num] = i;
+            }
+            return new int[0];
+        }
+
+        public int MaxProfit(int[] prices) {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+            for ( int i = 0; i < prices.Length; i++ ) {
+                int price = prices[i];
+                minPrice = Math.Min(minPrice, price);
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+            }
+            return maxProfit;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ DFBlazor/State/Weather.cs
@@ -0,0 +1,60 @@
+﻿using DFBlazor.Data;
+using Fluxor;
+
+namespace DFBlazor.State {
+    public class FetchWeatherAction {
+    }
+
+    public class FetchDataResultAction {
+        public IEnumerable<WeatherForecast> Forecasts { get; }
+        public FetchDataResultAction(IEnumerable<WeatherForecast> forecasts) {
+            Forecasts = forecasts;
+        }
+    }
+
+    [FeatureState]
+    public class WeatherState {
+        public bool IsLoading { get; }
+        public IEnumerable<WeatherForecast> Forecasts { get; }
+
+        public WeatherState() { }
+        public WeatherState(bool isLoading, IEnumerable<WeatherForecast> forecasts) {
+            IsLoading = isLoading;
+            Forecasts = forecasts;
+        }
+
+    }
+
+    public static class Reducers {
+
+        [ReducerMethod]
+        public static WeatherState ReduceFetchDataAction(WeatherState state, FetchWeatherAction action) =>
+            new WeatherState(isLoading: true, forecasts: null);
+
+        [ReducerMethod]
+        public static WeatherState ReduceFetchDataResultAction(WeatherState state, FetchDataResultAction action) =>
+            new WeatherState(isLoading: false, forecasts: action.Forecasts);
+    }
+
+    public class Effects {
+
+        //private readonly IHttpClientFactory _httpClientFactory;
+
+        //public Effects(IHttpClientFactory httpClientFactory) {
+        //    _httpClientFactory = httpClientFactory;
+        //} 
+
+
+        [EffectMethod]
+        public async Task HandleFetchWeatherAction(FetchWeatherAction action, IDispatcher dispatcher) {
+
+            WeatherForecastService forcastService = new WeatherForecastService();
+            var forecasts = await forcastService.GetForecastAsync(DateTime.Now);
+            //var client = _httpClientFactory.CreateClient();
+            //var forecasts = await client.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+            if (forecasts is not null) {
+                dispatcher.Dispatch(new FetchDataResultAction(forecasts: forecasts!));
+            }
+        }
+    }
+}
